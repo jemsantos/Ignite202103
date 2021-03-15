1e06bd71aac2fed2cf39318acb57dc63c1661667
José Santos <71701079@aluno.faculdadecotemig.br>
2021-03-14 23:34:38 -0300

    Código final da aula: N01 - Configurando o projeto - Adicionando o Nodemon na aplicação

--- nivel_01/fundamentos-nodejs/src/index.js
+++ nivel_01/fundamentos-nodejs/src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", (request, response) => {
-  return response.json({ message: "Hello World Ignite!" });
+  return response.json({ message: "Hello World Ignite - Fundamentos de NodeJS!" });
 });
 
 //localhost:3333
