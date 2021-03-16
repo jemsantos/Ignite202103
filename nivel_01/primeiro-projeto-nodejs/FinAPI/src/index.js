69228df8070784ec13724f7ef4e2c6cb1c08a86c
José Santos <71701079@aluno.faculdadecotemig.br>
2021-03-16 00:31:28 -0300

    Código final da aula: N02 - sem titulo - Cadastro de conta

--- nivel_01/primeiro-projeto-nodejs/FinAPI/src/index.js
+++ nivel_01/primeiro-projeto-nodejs/FinAPI/src/index.js
@@ -1,10 +1,30 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/courses", (request, response) => {
+const customers = [];
+
+/**
+ * cpf - string
+ * name - string
+ * id - uuid
+ * statement []
+ */
+app.post("/account", (request, response) => {
+  const { cpf, name } = request.body;
+  const id = uuidv4();
+
+  customers.push({
+    id,
+    cpf,
+    name,
+    statement: [],
+  });
+
+  return response.status(201).send();
 });
 
 app.listen(3333);
