4df61481ed93bbaefe0e8853076a63b4d425817b
José Santos <71701079@aluno.faculdadecotemig.br>
2021-03-22 18:34:16 -0300

    Código final da aula: N02 - sem titulo - Validando CPF existente

--- nivel_01/primeiro-projeto-nodejs/FinAPI/src/index.js
+++ nivel_01/primeiro-projeto-nodejs/FinAPI/src/index.js
@@ -7,18 +7,20 @@ app.use(express.json());
 
 const customers = [];
 
-/**
- * cpf - string
- * name - string
- * id - uuid
- * statement []
- */
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
-  const id = uuidv4();
+
+  /* validar se o CPF ainda não está cadastrado */
+  const customerAlreadyExists = customers.some(
+    customer => customer.cpf === cpf
+  );
+
+  if (customerAlreadyExists) {
+    return response.status(400).json({ error: "Customer already exists!" });
+  }
 
   customers.push({
-    id,
+    id: uuidv4(),
     cpf,
     name,
     statement: [],
