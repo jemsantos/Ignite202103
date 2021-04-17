edf88ef9de159aae7c4224e6e01bf57c11ca737b
José Santos <eustaquio.cadastros@gmail.com>
2021-04-16 21:40:26 -0300

    Código final da aula: N02 - Continuação da aplicação - Criando repositório de especificação

--- nivel_02/iniciando-a-api/rentalx/src/modules/cars/services/CreateSpecificationService.ts
+++ nivel_02/iniciando-a-api/rentalx/src/modules/cars/services/CreateSpecificationService.ts
@@ -1,6 +1,27 @@
+import { ISpecificationsRepository } from "../repositories/ISpecificationsRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
 class CreateSpecificationService {
-  execue() {
+  constructor(private specificationsRepository: ISpecificationsRepository) {
     console.log("TODO");
+  }
+
+  execute({ name, description }: IRequest): void {
+    const specificationAlreadExists = this.specificationsRepository.findByName(
+      name
+    );
+
+    if (specificationAlreadExists) {
+      throw new Error("Specification already exists!");
+    }
+
+    this.specificationsRepository.create({
+      name,
+      description,
+    });
   }
 }
 
