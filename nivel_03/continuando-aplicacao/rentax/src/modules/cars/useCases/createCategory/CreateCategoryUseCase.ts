4f88ad868f2026dadb3c4dfb755213f8b008277f
José Santos <eustaquio.cadastros@gmail.com>
2021-06-02 22:38:29 -0300

    Código final da aula: N03 - Trabalhando com Banco de Dados - Refatorando o caso de uso de categoria

--- nivel_03/continuando-aplicacao/rentax/src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ nivel_03/continuando-aplicacao/rentax/src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -11,8 +11,10 @@ class CreateCategoryUseCase {
     console.log("'CreateCategoryService' criado");
   }
 
-  execute({ name, description }: IRequest): void {
-    const categoryAlreadyExists = this.categoriesRepository.findByName(name);
+  async execute({ name, description }: IRequest): Promise<void> {
+    const categoryAlreadyExists = await this.categoriesRepository.findByName(
+      name
+    );
 
     if (categoryAlreadyExists) {
       throw new Error("Category already exists!");
