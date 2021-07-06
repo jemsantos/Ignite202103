24eea6b8837537b78ef1076d790245a531172cf3
José Santos <eustaquio.cadastros@gmail.com>
2021-07-05 22:27:46 -0300

    Código final da aula: N04 - Carros - Caso de uso do cadastro de imagens do carro

--- nivel_04/testes-e-regras-de-negocio/rentx/src/config/upload.ts
+++ nivel_04/testes-e-regras-de-negocio/rentx/src/config/upload.ts
@@ -8,7 +8,7 @@ export default {
   upload(folder: string) {
     return {
       storage: multer.diskStorage({
-        destination: tmpFolder,
+        destination: resolve(__dirname, "..", "..", folder),
         filename: (request, file, callback) => {
           const fileHash = crypto.randomBytes(16).toString("hex");
           const fileName = `${fileHash}-${file.originalname}`;
