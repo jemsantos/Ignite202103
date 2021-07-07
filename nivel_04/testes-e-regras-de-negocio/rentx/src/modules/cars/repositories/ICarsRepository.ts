597af7086a96694536e3a3f8b7238f1a928f0cb9
José Santos <eustaquio.cadastros@gmail.com>
2021-07-07 00:12:47 -0300

    Código final da aula: N04 - Aluguel - Criando os testes do aluguel

--- nivel_04/testes-e-regras-de-negocio/rentx/src/modules/cars/repositories/ICarsRepository.ts
+++ nivel_04/testes-e-regras-de-negocio/rentx/src/modules/cars/repositories/ICarsRepository.ts
@@ -2,6 +2,8 @@ import { ICreateCarDTO } from "../dtos/ICreateCarDTO";
 import { Car } from "../infra/typeorm/entities/Car";
 
 interface ICarsRepository {
+  /* findByName(name: string): Promise<Car>;
+  list(): Promise<Car[]>; */
   create(data: ICreateCarDTO): Promise<Car>;
   findByLicensePlate(license_plate: string): Promise<Car>;
   findAvailable(
